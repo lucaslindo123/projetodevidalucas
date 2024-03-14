de43a2a529f287aeb1b90fde8d1a3b9c232f6bdb
Lucas Miranda <106752991+lucaslindo123@users.noreply.github.com>
2024-03-14 07:58:56 -0300

    js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const botoes = document.querySelectorAll (".botao")
+const botoes = document.querySelectorAll(".botao")
 
 for (let i = 0; i < botoes.length; i++ ) {
 
-    botoes[i].onlick = function () {
+    botoes[i].onclick = function () {
 
         for (let j = 0; j < botoes.length; j++) {
-            botoes [j].classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo")
