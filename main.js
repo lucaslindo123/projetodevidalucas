b899e7de0f1f2aada4ee1353d0360b3867e790a1
Lucas Miranda <106752991+lucaslindo123@users.noreply.github.com>
2024-03-26 12:01:53 -0300

    aula de cria

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao")
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++ ) {
 
@@ -6,8 +7,12 @@ for (let i = 0; i < botoes.length; i++ ) {
 
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+
+
         }
 
         botoes[i].classList.add("ativo")
+        textos[i].classList.add("ativo")
     }
 }
